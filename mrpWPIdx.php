5cb53432ecea197eb8b65189c7be67ee7246558b
Vas Skrypnyk <bill@myrealpage.com>
2016-09-08 21:00:51 +0000

    Added PHP version to the debug dump

--- mrpWPIdx.php
+++ mrpWPIdx.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: myRealPage IDX Listings
  * Description: Embeds myRealPage IDX and Listings solution into WordPress. Uses shortcodes. Create a post or page and use integrated shortcode button to launch myRealPage Listings Shortcode Wizard and generate a shortcode based on your choice of listing content, as well as functional and visual preferences.
- * Version: 0.9.7
+ * Version: 0.9.8
  * Author: myRealPage (support@myrealpage.com)
  * Author URI: http://myrealpage.com
  **/
@@ -469,6 +469,7 @@ if (!class_exists('MRPListing')) {
             $debug = $this->logger->getNotifications(true);
             if (strlen($debug)) {
                 $content .= "<!-- MRP WPIDX DEBUG -->\n";
+                $content .= "<!-- PHP VERSION: " . phpversion() . " -->\n";
                 $content .= "<!-- $debug -->\n";
             }
             echo $content;
