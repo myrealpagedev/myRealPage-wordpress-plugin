ea248c4a996b56c8a7421182426e82cdbb6f2e76
Eric Blanchette <eric.blanchette@digitalmatrices.com>
2022-05-10 16:48:46 -0400

    Bug fix location rewrite

--- lib/Proxy.php
+++ lib/Proxy.php
@@ -72,9 +72,9 @@ class Proxy {
         // handle redirects
         if ($response->isRedirect() && $response->hasHeader("Location")) {
 
-            $location = $response->getHeader('Location');
-			if( preg_match('@http(s?):\/\/([^\/]+\.myrealpage.com)\/.*@', $location, $matches)) {
-				$location = preg_replace('@http(s?)://(.+?)/(.*)@', ( $this->isSecure() ? 'https://' : 'http://' ) .$_SERVER['HTTP_HOST'].'/$3', $location);
+            $location = trim($response->getHeader('Location'));
+			if( preg_match('@^http(s?):\/\/([^\/]+\.myrealpage.com)\/.*@', $location, $matches)) {
+                $location = preg_replace('@^http(s?):\/\/(.+?)\/(.*)@', ( $this->isSecure() ? 'https://' : 'http://' ) .$_SERVER['HTTP_HOST'].'/$3', $location);
 			}
 
             header("HTTP/1.1 " . $response->getResponseCodeWithString());
